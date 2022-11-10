5b6193fde3fa8d9167eda2bc3f5180d99f92d215
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-10 10:35:00 +0100

    ajout des routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
+import CadeauView from "../views/CadeauView.vue"
+import ComView from "../views/ComView.vue"
+import ConnexionView from "../views/ConnexionView.vue"
+import ContactView from "../views/ContactView.vue"
+import CreaView from "../views/CreaView.vue"
+import DevView from "../views/DevView.vue"
+import EventView from "../views/EventView.vue"
+import MentionsLegalesView from "../views/MentionsLegalesView.vue"
+import SecretView from "../views/SecretView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/',  name: 'home', component: HomeView },
+    { path: '/',  name: 'cadeau', component: CadeauView },
+    { path: '/home',  name: 'home', component: HomeView },
+    { path: '/com',  name: 'com', component: ComView },
+    { path: '/connexion',  name: 'connexion', component: ConnexionView },
+    { path: '/contact',  name: 'contact', component: ContactView },
+    { path: '/crea',  name: 'crea', component: CreaView },
+    { path: '/dev',  name: 'dev', component: DevView },
+    { path: '/event',  name: 'event', component: EventView },
+    { path: '/mentionslegales',  name: 'mentionslegales', component: MentionsLegalesView },
+    { path: '/secret',  name: 'secret', component: SecretView },
   ]
 })
 
