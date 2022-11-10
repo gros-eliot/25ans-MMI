aee0da74c35ec82c59c2781177d2029e1d901ef6
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-10 10:34:36 +0100

    Intégration icons logo

---
+++ src/assets/components/icons/logo.vue
@@ -0,0 +1,17 @@
+<template>
+    <svg
+  width="40"
+  height="40"
+  viewBox="0 0 40 39"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  preserveAspectRatio="none"
+>
+  <path
+    d="M20 2L11 36.6804L2 16.863L13.6842 25.0137L7.36842 6.6347L20 20.5388L33.1053 5.6758L26.9474 24.6941L38 16.863L29.3158 37L20 2Z"
+    stroke="black"
+    stroke-width="3"
+    stroke-linejoin="round"
+  ></path>
+</svg>
+</template>
