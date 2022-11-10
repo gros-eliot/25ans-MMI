568f27a03a2f3a22120fa59b1201b66307542256
gros-eliot <95086520+gros-eliot@users.noreply.github.com>
2022-11-10 08:20:49 +0100

    initialisation 25 ans

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+//import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <h1 class="text-2xl font-bold text-red-500">Hello world!</h1>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
