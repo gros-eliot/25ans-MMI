c05798d8f0c8a345ac138bc11517552cc09331c3
gros-eliot <95086520+gros-eliot@users.noreply.github.com>
2022-11-10 08:40:16 +0100

    PPNeueMontreal import

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <h1 class="text-2xl font-bold text-red-500">Hello world!</h1>
+  <h1 class="text-2xl text-red-500 font-neuemontreal">Hello world!</h1>
   <router-view></router-view>
 </template>
 
