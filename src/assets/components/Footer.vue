2dc9576a702dd852269118cdb15dc51a9a6ae884
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-10 11:29:04 +0100

    Intégration Footer

---
+++ src/assets/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-noir p-12">
+        <div class="md:flex items-center justify-between mx-10">
+            <div>
+                <router-link to="/contact">
+                <p class="footer">CONTACT</p>
+                </router-link>
+                <router-link to="/mentionslegales">
+                <p class="footer my-5">MENTIONS LEGALES</p>
+                </router-link>
+                <a to="/">
+                <p class="footer my-5">JPO 2022</p>
+                </a>
+            </div>
+        <a href="https://www.instagram.com/mmi25ans/">
+            <img src="../../../public/instagram.svg" alt="logo instagram">
+        </a>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
