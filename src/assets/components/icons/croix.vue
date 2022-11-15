a571d617362d866391548eb7ce0edecf542a9801
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-15 15:38:24 +0100

    Création icon croix

---
+++ src/assets/components/icons/croix.vue
@@ -0,0 +1,19 @@
+<template>
+<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="w-6 h-6 relative"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <path
+    d="M6 18L18 6M6 6L18 18"
+    stroke="white"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+</svg>
+</template>
