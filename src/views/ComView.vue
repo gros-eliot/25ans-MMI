da6dee9eb8286e537d6fa90b2f9808c3da396b59
llobato <louplelpro@gmail.com>
2022-11-15 15:13:09 +0100

    mise en forme du text présentation salle com

--- src/views/ComView.vue
+++ src/views/ComView.vue
@@ -3,9 +3,28 @@
 
 <template>
     <div>
-        <h1 class="font-inter font-black text-8xl
-        ">
+        <h1 class="font-inter font-black text-8xl w-full flex justify-center mt-28">
             LA SALLE COM
         </h1>
+    </div>
+    <div>
+        <!-- Composents plan -->
+    </div>
+    <div>
+        <!--Ligne et 25-->
+    </div>
+
+    <div class="max-w-4xl">
+        <div class="">
+            <h2 class="font-inter font-black text-7xl">
+                Communication au MMI...
+            </h2>
+            <p class="font-neuemontreal text-2xl">
+                Le pôle communication de MMI fait partie des trois grandes thématiques qu’aborde MMI dans son cursus. La
+                communication est très importante dans le milieu du web car elle permet aux différents projets de vivre
+                et de se faire une place dans les esprits. De plus, la communication régie toutes les parties qui
+                précèdent le lancement d’un projet, des analyses concurrentielles et la gestion du budget.
+            </p>
+        </div>
     </div>
 </template>
