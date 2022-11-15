7784dee4b5ab0443a811eb25f767fa0c84112d05
llobato <louplelpro@gmail.com>
2022-11-15 14:53:37 +0100

    Titre salle com

--- src/views/ComView.vue
+++ src/views/ComView.vue
@@ -3,7 +3,8 @@
 
 <template>
     <div>
-        <h1 class="font-inter font-black text-8xl">
+        <h1 class="font-inter font-black text-8xl
+        ">
             LA SALLE COM
         </h1>
     </div>
