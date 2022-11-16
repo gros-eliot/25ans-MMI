a14d73998ef90383c4acba5dae52e71b55db3e10
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-16 11:25:14 +0100

    responsive page com

--- src/views/ComView.vue
+++ src/views/ComView.vue
@@ -1,8 +1,9 @@
 <script setup></script>
 
 <template>
+    <div class="">
     <div>
-        <h1 class="font-inter font-black text-8xl w-full flex justify-center mt-28">
+        <h1 class="mmi-h1 text-center">
             LA SALLE COM
         </h1>
     </div>
@@ -10,11 +11,11 @@
         <img class="m-auto" src="/oeuvres.jpg">
     </div>
 
-    <div class="flex items-center justify-between pr-16 my-36">
-        <div class="max-w-3xl ml-28">
-            <div class="">
-                <h2 class="font-inter font-black text-7xl">Communication au MMI...</h2>
-                <p class="text-4xl">
+    <div class="flex items-center w-full p-5 gap-10">
+        <div class=" ">
+            <div class="max-w-5xl">
+                <h2 class=" text-center lg:text-left font-inter font-black text-6xl lg:text-7xl">Communication au&nbsp;MMI...</h2>
+                <p class="mmi-texte ">
                     Le pôle communication de MMI fait partie des trois grandes thématiques
                     qu’aborde MMI dans son cursus. La communication est très importante dans
                     le milieu du web car elle permet aux différents projets de vivre et de
@@ -24,8 +25,8 @@
                 </p>
             </div>
         </div>
-        <div>
-                <p class="text-8xl font-thin">
+        <div class="m-auto hidden lg:block w-fit mt-96">
+                <p class="text-7xl xl:text-8xl my-10 font-normal ">
                     2525252525 <br />
                     2525252525 <br />
                     2525252525
@@ -35,12 +36,12 @@
         <div>
         <!--ligne rouge-->
     </div>
-    <div class="flex justify-center items-center gap-24">
-        <img src="../../public/images/D-Malsot 1.png" alt="Photo de David Malsot" class="max-w-lg" />
+    <div class="lg:flex justify-center items-center">
+        <img src="../../public/images/D-Malsot 1.png" alt="Photo de David Malsot" class="max-w-lg m-auto" />
 
-        <div class="max-w-xl">
-            <h3 class="font-inter font-black text-5xl">M.MALSOT</h3>
-            <p class="text-4xl">
+        <div class="m-auto p-5">
+            <h3 class="font-inter text-center font-black text-5xl">M.MALSOT</h3>
+            <p class="mmi-texte">
                 Monsieur David Malsot est l’un des professeurs principaux de MMI mais
                 est aussi en grande partie chargé de l’initiation à la communication
                 pour les élèves de première année. Il est un professeur important pour
@@ -51,17 +52,17 @@
         </div>
     </div>
 
-    <div class="flex items-center justify-between pr-16 my-36">
+    <div class="flex items-center w-full p-5 gap-10">
         <div>
-            <p class="text-8xl font-thin">
+            <p class="text-7xl hidden lg:block xl:text-8xl my-10 font-normal ">
                 2525252525 <br />
                 2525252525 <br />
                 2525252525
             </p>
         </div>
-        <div class="flex flex-col items-center gap-6">
-            <h3 class="font-inter font-black text-5xl">AUTRES PROFESSEURS DE COM’</h3>
-            <div class="flex gap-9">
+        <div class="flex flex-col m-auto items-center gap-6">
+            <h3 class="font-inter font-black text-4xl xl:text-5xl">AUTRES PROFESSEURS DE COM’</h3>
+            <div class="grid grid-cols-galerie-profs gap-9 w-full">
                 <div class="flex flex-col gap-7 items-center">
                     <img src="../../public/images/JC-Domenget 1.png" alt="photo Monsieur Domenget" />
                     <p class="font-neuemontreal text-2xl">Jean-Claude Domenget</p>
@@ -70,7 +71,11 @@
                     <img src="../../public/images/S-Laurent 1.png" alt="Photo Monsieur Laurent" />
                     <p class="font-neuemontreal text-2xl">Stephane Laurent</p>
                 </div>
+               
+                
+                
             </div>
         </div>
+    </div>
     </div>
 </template>
