d92d125bbe493165028ceb6377460bd7c3ea8240
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-16 11:25:07 +0100

    changement nom classe uti

--- src/views/ConnexionView.vue
+++ src/views/ConnexionView.vue
@@ -3,7 +3,7 @@ import LoginLogout from "@/components/LoginLogout.vue";
 </script>
 
 <template>
-  <h1 class="titre">Connexion</h1>
+  <h1 class="mmi-h1">Connexion</h1>
   <div class="flex justify-center mt-10">
     <LoginLogout />
   </div>
