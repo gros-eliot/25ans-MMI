7e04c061272a0a8d842475ddf1c6e56aaba88c38
llobato <louplelpro@gmail.com>
2022-11-16 09:49:04 +0100

    Page créa

---
+++ src/views/CreaView.vue
@@ -0,0 +1,67 @@
+<script setup></script>
+
+<template>
+    <div>
+        <h1 class="font-inter font-black text-8xl w-full flex justify-center mt-28">
+            LA SALLE CREA
+        </h1>
+    </div>
+    <div>
+        <!-- Composents plan -->
+    </div>
+    <div>
+        <!--Ligne et 25-->
+    </div>
+
+    <div class="max-w-3xl ml-28">
+        <div class="">
+            <h2 class="font-inter font-black text-7xl">La création au MMI...</h2>
+            <p class="text-4xl">
+                Le pôle Création est celui qui contient les matières artistiques. Design, Graphisme, Motion Design,
+                Audiovisuel, Narration, toutes ces matières font partie de ce pôle. Le design est tout aussi important
+                que la communication et le développement. Par définition c’est la façade du web et souvent c’est ce que
+                l’on retient en premier d’un site internet. Mis à part ça, le design est une partie importante de notre
+                vie, les visuels sont importants pour communiquer avec les sociétés modernes.
+            </p>
+        </div>
+    </div>
+    <div>
+        <!--ligne rouge-->
+    </div>
+    <div class="flex justify-center gap-24">
+        <img src="../../public/images/X-SENENTE.jpg" alt="Photo de David Malsot" class="max-w-lg" />
+
+        <div class="max-w-xl">
+            <h3 class="font-inter font-black text-5xl">M.SENENTE</h3>
+            <p class="text-4xl">
+                Monsieur Xavier Senente est le professeur principal du pôle créatif de MMI. Il est chargé de
+                l’apprentissage des logiciels de design et de graphisme, mais aussi des logiciels de prototypages pour
+                la composition des maquettes de site internet. Il enseigne aussi l’art du branding (créer un logo, une
+                identité graphique... ) tout en apportant une culture artistique aux élèves.
+            </p>
+        </div>
+    </div>
+
+    <div class="flex items-center justify-between pr-16 my-36">
+        <div>
+            <p class="text-8xl font-thin">
+                2525252525 <br />
+                2525252525 <br />
+                2525252525
+            </p>
+        </div>
+        <div class="flex flex-col items-center gap-6">
+            <h3 class="font-inter font-black text-5xl">AUTRES PROFESSEURS DE COM’</h3>
+            <div class="flex gap-9">
+                <div class="flex flex-col gap-7 items-center">
+                    <img src="../../public/images/A-Lamboud_Durand.jpg" alt="photo Monsieur Domenget" />
+                    <p class="font-neuemontreal text-2xl">Sebastien AUGIER</p>
+                </div>
+                <div class="flex flex-col gap-7 items-center">
+                    <img src="../../public/images/S-Augier.jpg" alt="Photo Monsieur Laurent" />
+                    <p class="font-neuemontreal text-2xl">Alain LAMBOUX-DURAND</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
