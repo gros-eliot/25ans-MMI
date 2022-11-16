2583cb5a799617197febc312c1bae49fa3afdc7c
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-16 11:33:43 +0100

    responsive page dev

--- src/views/CreaView.vue
+++ src/views/CreaView.vue
@@ -1,8 +1,9 @@
 <script setup></script>
 
 <template>
+    <div class="">
     <div>
-        <h1 class="font-inter font-black text-8xl w-full flex justify-center mt-28">
+        <h1 class="mmi-h1 text-center ">
             LA SALLE CREA
         </h1>
     </div>
@@ -10,64 +11,61 @@
         <img class="m-auto" src="/oeuvres.jpg">
     </div>
 
-    <div class="flex items-center justify-between pr-16 my-36">
-        <div class="max-w-3xl ml-28">
-            <div class="">
-                <h2 class="font-inter font-black text-7xl">La création au MMI...</h2>
-                <p class="text-4xl">
-                    Le pôle Création est celui qui contient les matières artistiques. Design, Graphisme, Motion Design,
-                    Audiovisuel, Narration, toutes ces matières font partie de ce pôle. Le design est tout aussi important
-                    que la communication et le développement. Par définition c’est la façade du web et souvent c’est ce que
-                    l’on retient en premier d’un site internet. Mis à part ça, le design est une partie importante de notre
-                    vie, les visuels sont importants pour communiquer avec les sociétés modernes.
+    <div class="flex items-center w-full p-5 gap-10">
+        <div class=" ">
+            <div class="max-w-5xl">
+                <h2 class=" text-center lg:text-left font-inter font-black text-6xl lg:text-7xl">La création au&nbsp;MMI...</h2>
+                <p class="mmi-texte ">
+                    Le pôle Création est celui qui contient les matières artistiques. Design, Graphisme, Motion Design, Audiovisuel, Narration, toutes ces matières font partie de ce pôle. Le design est tout aussi important que la communication et le développement. Par définition c’est la façade du web et souvent c’est ce que l’on retient en premier d’un site internet. Mis à part ça, le design est une partie importante de notre vie, les visuels sont importants pour communiquer avec les sociétés modernes.
                 </p>
             </div>
         </div>
-        <div>
-            <p class="text-8xl font-thin">
-                2525252525 <br />
-                2525252525 <br />
-                2525252525
-            </p>
-        </div>
+        <div class="m-auto hidden lg:block w-fit mt-96">
+                <p class="text-7xl xl:text-8xl my-10 font-normal ">
+                    2525252525 <br />
+                    2525252525 <br />
+                    2525252525
+                </p>
+            </div>
     </div>
-    <div>
+        <div>
         <!--ligne rouge-->
     </div>
-    <div class="flex justify-center items-center gap-24">
-        <img src="../../public/images/X-SENENTE.jpg" alt="Photo de Xavier Senente" class="max-w-lg" />
+    <div class="lg:flex justify-center items-center">
+        <img src="/images/X-SENENTE.jpg" alt="Photo de Xavier Senente" class="max-w-lg m-auto" />
 
-        <div class="max-w-xl">
-            <h3 class="font-inter font-black text-5xl">M.SENENTE</h3>
-            <p class="text-4xl">
-                Monsieur Xavier Senente est le professeur principal du pôle créatif de MMI. Il est chargé de
-                l’apprentissage des logiciels de design et de graphisme, mais aussi des logiciels de prototypages pour
-                la composition des maquettes de site internet. Il enseigne aussi l’art du branding (créer un logo, une
-                identité graphique... ) tout en apportant une culture artistique aux élèves.
+        <div class="m-auto p-5 lg:p-10">
+            <h3 class="font-inter text-center font-black text-5xl">M.SENENTE</h3>
+            <p class="mmi-texte">
+                Monsieur Xavier Senente est le professeur principal du pôle créatif de MMI. Il est chargé de l’apprentissage des logiciels de design et de graphisme, mais aussi des logiciels de prototypages pour la composition des maquettes de site internet. Il enseigne aussi l’art du branding (créer un logo, une identité graphique... ) tout en apportant une culture artistique aux élèves.
             </p>
         </div>
     </div>
 
-    <div class="flex items-center justify-between pr-16 my-36">
+    <div class="flex items-center w-full p-5 gap-10">
         <div>
-            <p class="text-8xl font-thin">
+            <p class="text-7xl hidden lg:block xl:text-8xl my-10 font-normal ">
                 2525252525 <br />
                 2525252525 <br />
                 2525252525
             </p>
         </div>
-        <div class="flex flex-col items-center gap-6">
-            <h3 class="font-inter font-black text-5xl">AUTRES PROFESSEURS DE COM’</h3>
-            <div class="flex gap-9">
+        <div class="flex flex-col m-auto items-center gap-6">
+            <h3 class="font-inter font-black text-4xl xl:text-5xl">AUTRES PROFESSEURS DE CREA</h3>
+            <div class="grid grid-cols-galerie-profs gap-9 w-full">
                 <div class="flex flex-col gap-7 items-center">
-                    <img src="../../public/images/A-Lamboud_Durand.jpg" alt="photo Monsieur Lamboud Durant" />
+                    <img src="/images/S-Augier.jpg" alt="photo de Sébastien AUGIER" />
+                    <p class="font-neuemontreal text-2xl">Sébastien AUGIER</p>
+                </div>
+                <div class="flex flex-col gap-7 items-center">
+                    <img src="/images/A-Lamboud_Durand.jpg" alt="Photo Monsieur Alain LAMBOUX-DURAND" />
                     <p class="font-neuemontreal text-2xl">Alain LAMBOUX-DURAND</p>
                 </div>
-                <div class="flex flex-col gap-7 items-center">
-                    <img src="../../public/images/S-Augier.jpg" alt="Photo Monsieur Augier" />
-                    <p class="font-neuemontreal text-2xl">Sebastien AUGIER</p>
-                </div>
+               
+                
+                
             </div>
         </div>
+    </div>
     </div>
 </template>
