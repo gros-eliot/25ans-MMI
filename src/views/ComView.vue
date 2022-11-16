c542054025fa5a3ad371b6c785240c1af0b90bcd
llobato <louplelpro@gmail.com>
2022-11-16 08:49:23 +0100

    mid mise en page com

--- src/views/ComView.vue
+++ src/views/ComView.vue
@@ -1,73 +1,63 @@
 <script setup></script>
 
 <template>
-  <div>
-    <h1 class="font-inter font-black text-8xl w-full flex justify-center mt-28">
-      LA SALLE COM
-    </h1>
-  </div>
-  <div>
-    <!-- Composents plan -->
-  </div>
-  <div>
-    <!--Ligne et 25-->
-  </div>
-
-  <div class="max-w-3xl ml-28">
-    <div class="">
-      <h2 class="font-inter font-black text-7xl">Communication au MMI...</h2>
-      <p class="text-4xl">
-        Le pôle communication de MMI fait partie des trois grandes thématiques
-        qu’aborde MMI dans son cursus. La communication est très importante dans
-        le milieu du web car elle permet aux différents projets de vivre et de
-        se faire une place dans les esprits. De plus, la communication régie
-        toutes les parties qui précèdent le lancement d’un projet, des analyses
-        concurrentielles et la gestion du budget.
-      </p>
-    </div>
-  </div>
-  <div>
-    <!--ligne rouge-->
-  </div>
-  <div class="flex justify-center gap-24">
-    <img
-      src="../../public/images/D-Malsot 1.png"
-      alt="Photo de David Malsot"
-      class="max-w-lg"
-    />
-
-    <div class="max-w-xl">
-      <h3 class="font-inter font-black text-5xl">M.MALSOT</h3>
-      <p class="text-4xl">
-        Monsieur David Malsot est l’un des professeurs principaux de MMI mais
-        est aussi en grande partie chargé de l’initiation à la communication
-        pour les élèves de première année. Il est un professeur important pour
-        le département depuis ses multiples implications que ce soit dans
-        l’enseignement ou encore dans les évènements en rapport avec le
-        département.
-      </p>
-    </div>
-  </div>
-
-  <div>
     <div>
-      <h3 class="font-inter font-black text-5xl">AUTRES PROFESSEURS DE COM’</h3>
-      <div class="flex">
-        <div>
-          <img
-            src="../../public/images/JC-Domenget 1.png"
-            alt="photo Monsieur Domenget"
-          />
-          <p>Jean-Claude Domenget</p>
-        </div>
-        <div>
-          <img
-            src="../../public/images/S-Laurent 1.png"
-            alt="Photo Monsieur Laurent"
-          />
-          <p>Stephane Laurent</p>
-        </div>
-      </div>
+        <h1 class="font-inter font-black text-8xl w-full flex justify-center mt-28">
+            LA SALLE COM
+        </h1>
     </div>
-  </div>
+    <div>
+        <!-- Composents plan -->
+    </div>
+    <div>
+        <!--Ligne et 25-->
+    </div>
+
+    <div class="max-w-3xl ml-28">
+        <div class="">
+            <h2 class="font-inter font-black text-7xl">Communication au MMI...</h2>
+            <p class="text-4xl">
+                Le pôle communication de MMI fait partie des trois grandes thématiques
+                qu’aborde MMI dans son cursus. La communication est très importante dans
+                le milieu du web car elle permet aux différents projets de vivre et de
+                se faire une place dans les esprits. De plus, la communication régie
+                toutes les parties qui précèdent le lancement d’un projet, des analyses
+                concurrentielles et la gestion du budget.
+            </p>
+        </div>
+    </div>
+    <div>
+        <!--ligne rouge-->
+    </div>
+    <div class="flex justify-center gap-24">
+        <img src="../../public/images/D-Malsot 1.png" alt="Photo de David Malsot" class="max-w-lg" />
+
+        <div class="max-w-xl">
+            <h3 class="font-inter font-black text-5xl">M.MALSOT</h3>
+            <p class="text-4xl">
+                Monsieur David Malsot est l’un des professeurs principaux de MMI mais
+                est aussi en grande partie chargé de l’initiation à la communication
+                pour les élèves de première année. Il est un professeur important pour
+                le département depuis ses multiples implications que ce soit dans
+                l’enseignement ou encore dans les évènements en rapport avec le
+                département.
+            </p>
+        </div>
+    </div>
+
+    <div>
+        <div>
+            <h3 class="font-inter font-black text-5xl">AUTRES PROFESSEURS DE COM’</h3>
+            <div class="flex">
+                <div>
+                    <img src="../../public/images/JC-Domenget 1.png" alt="photo Monsieur Domenget" />
+                    <p>Jean-Claude Domenget</p>
+                </div>
+                <div>
+                    <img src="../../public/images/S-Laurent 1.png" alt="Photo Monsieur Laurent" />
+                    <p>Stephane Laurent</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
