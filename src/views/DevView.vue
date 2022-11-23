c03f75d1637b8ba683ccfbe8c83e8a9f5331d92c
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-11-23 09:51:30 +0100

    responsive page dev

--- src/views/DevView.vue
+++ src/views/DevView.vue
@@ -1,18 +1,21 @@
 <script setup></script>
 
 <template>
-  <div class="my-10">
-    <img class="m-auto" src="/oeuvres.jpg" />
-  </div>
+  <div class="">
+    <div class="my-10">
+      <img class="m-auto" src="/oeuvres.jpg" />
+    </div>
 
-  <div class="flex items-center justify-between pr-16 my-36">
-    <div class="max-w-3xl ml-28">
-      <div class="">
-        <h2 class="font-inter font-black text-7xl">
-          Le développement au MMI...
-        </h2>
-        <p class="text-4xl">
-          Le pôle développement est aussi important dans le cursus MMI. Au cours
+    <div class="flex items-center w-full p-5 gap-10">
+      <div class=" ">
+        <div class="max-w-5xl">
+          <h2
+            class="text-center lg:text-left font-inter font-black text-6xl lg:text-7xl"
+          >
+            Le développement au&nbsp;MMI...
+          </h2>
+          <p class="mmi-texte">
+            Le pôle développement est aussi important dans le cursus MMI. Au cours
           des trois années de BUT les élèves apprennent à développer le web.
           Ceci grâce à l’apprentissage de différents langages de programmation
           et d’intégration comme l’HTML, le CSS, le Javascript, et d’autres.
@@ -20,51 +23,52 @@
           cursus de MMI. Le développement est la base de tout ce qui est présent
           sur le web, il est donc important de s’y pencher pour mieux comprendre
           ce milieu et être capable d’y évoluer.
+          </p>
+        </div>
+      </div>
+      <div class="m-auto hidden lg:block w-fit mt-96">
+        <p class="text-7xl xl:text-8xl my-10 font-normal">
+          2525252525 <br />
+          2525252525 <br />
+          2525252525
         </p>
       </div>
     </div>
     <div>
-      <p class="text-8xl font-thin">
-        2525252525 <br />
-        2525252525 <br />
-        2525252525
-      </p>
+      <!--ligne rouge-->
     </div>
-  </div>
-  <div>
-    <!--ligne rouge-->
-  </div>
-  <div class="flex justify-center items-center my-10 gap-24">
-    <img
+    <div class="lg:flex justify-center items-center">
+      <img
       src="/images/JM-Fino.jpg"
       alt="Photo de Jean Marie Fino"
-      class="max-w-lg"
+      class="max-w-lg m-auto"
     />
 
-    <div class="max-w-xl">
-      <h3 class="font-inter font-black text-5xl">M.FINO</h3>
-      <p class="text-4xl">
-        Monsieur Jean-Marie Fino est un membre important de MMI car il est l’un
+      <div class="m-auto p-5">
+        <h3 class="font-inter text-center font-black text-5xl">M.FINO</h3>
+        <p class="mmi-texte">
+          Monsieur Jean-Marie Fino est un membre important de MMI car il est l’un
         des fondateurs du département, à l’époque MMI s’appelait SRC. Il est un
         bon représentant du pôle développement car il est lui-même développeur
         full stack. Il est chargé de l’apprentissage des langages d’intégration
         et de développement web.
-      </p>
+        </p>
+      </div>
     </div>
-  </div>
 
-  <div class="flex items-center justify-between pr-16 my-36">
-    <div>
-      <p class="text-8xl font-thin">
-        2525252525 <br />
-        2525252525 <br />
-        2525252525
-      </p>
-    </div>
-    <div class="flex flex-col items-center gap-6">
-      <h3 class="font-inter font-black text-5xl">AUTRES PROFESSEURS DE DEV’</h3>
-      <div class="flex gap-9">
-        <div class="flex flex-col gap-5">
+    <div class="flex items-center w-full p-5 gap-10">
+      <div>
+        <p class="text-7xl hidden lg:block xl:text-8xl my-10 font-normal">
+          2525252525 <br />
+          2525252525 <br />
+          2525252525
+        </p>
+      </div>
+      <div class="flex flex-col m-auto items-center gap-6">
+        <h3 class="font-inter font-black text-4xl xl:text-5xl">
+          AUTRES PROFESSEURS DE DEV’
+        </h3>
+        <div class="grid grid-cols-galerie-profs gap-9 w-full">
           <div class="flex flex-col gap-7 items-center">
             <img src="/images/P-Chatonnay.jpg" alt="photo Monsieur Chatonnay" />
             <p class="font-neuemontreal text-2xl">Pascal Chatonnay</p>
@@ -73,8 +77,6 @@
             <img src="/images/PASDEPHOTO.png" alt="photo inconnue" />
             <p class="font-neuemontreal text-2xl">Pierre Pracht</p>
           </div>
-        </div>
-        <div class="flex flex-col gap-5">
           <div class="flex flex-col gap-7 items-center">
             <img src="/images/H-Mabed.jpg" alt="Photo Monsieur Mabed" />
             <p class="font-neuemontreal text-2xl">Hakim Mabed</p>
