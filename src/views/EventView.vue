cddba4697dbf5685d944884176452179cdae1707
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-01-12 11:22:10 +0100

    début codage page event

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,21 +1,55 @@
 <template>
     <h2 class="bg-cover text-center py-60 bg-[url('/images/event.jpg')] mmi-h2 text-white">SAVE THE DATE 11 ET 12 MARS</h2>
-    <div class="flex mt-8">
-        <div class="flex flex-col">
-        <p class="ml-5 mmi-texte">Pour fêter ses 25 ans, MMI organise un événement de rencontre le 11 et 12 mars.</p>
-        <p class="ml-5 mr-16 mmi-texte">Le samedi 11 mars 2023, rendez-vous à <strong>21h dans la salle 006</strong> du département pour
-            une soirée originale en coopération avec le défi 24h ! Un moment de partage unique entre anciens étudiants, étudiants actuels
-            et professeurs autour d'une collation et de musique ! 
-        </p>
-        <p class="ml-5 mr-16 mmi-texte">Mais la fête de s'arrête pas là ! Le dimanche 12 mars,  
-        </p>
+    <div class="2xl:flex mt-8">
+        <div class="flex flex-col gap-3 ">
+          <h3 class="mx-auto mmi-h2">L'ÉVÉNEMENT DES 25 ANS</h3>
+          <div class="2xl:flex items-center ml-5 2xl:mt-16 2xl:ml-0">
+              <img
+                  src="../../public/images/funny-version/OFFF.jpg"
+                  alt="Photo OFFF"
+                  class="m-auto 2xl:mmi-photo-4 mmi-photo-2 -rotate-90  "
+                />
+            <div class="2xl:mr-10">
+              <div>
+                <p class=" mmi-texte">Pour fêter ses 25 ans, MMI organise un événement de rencontre le 11 et 12 mars.</p>
+                <p class=" mr-16 mmi-texte">Tous les MMI, anciens, actuels et professeurs, sont invités le samedi 11 mars 
+                  2023 ! rendez-vous à <strong>21h dans la salle 006</strong> du département pour une soirée originale en 
+                  coopération avec le défi 24h ! Un moment de partage unique entre anciens étudiants, étudiants actuels
+                    et professeurs autour d'une collation et de musique ! 
+                </p>
+              </div>
+              <div>          
+                <p class="mr-16 mmi-texte">Mais la fête de s'arrête pas là ! Le dimanche 12 mars, venez participer à la remise
+                des prix du défi 24h et du concours des 25 ans ! Un appéro dinatoire permettra de cloturer en beauté ce 
+                week-end unique de partage et de souvenirs en mémoire de ces 25 magnifiques années passées ensemble à 
+                construire le département MMI que nous connaissons aujourd'hui !  
+              </p>
+              </div>
+            </div>
+          </div>
         </div>
     <!--2525252525-->
+    <div class="flex items-center justify-center">
+      <h3 class="mx-auto 2xl:hidden font-inter font-black text-6xl lg:text-8xl tracking-tighter">MERCI !!</h3>
       <div class="ml-auto w-full md:w-fit text-5xl md:text-8xl my-10">
-        <div class="flex-col flex max-w-xl w-full boxDefilementText">
+        <div class="flex-col gap-16 flex max-w-xl w-full boxDefilementText">
           <p class="font-thin object1">
             252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525252525
           </p>
+          <div class="md:flex mx-auto">
+            <img
+                    src="../../public/images/funny-version/Profs.jpg"
+                    alt="Photo des professeurs"
+                    class="mmi-photo-2  "
+                  />
+            <img
+            src="../../public/images/funny-version/Senente2.jpeg"
+            alt="Photo Monsieur Senente"
+            class="mmi-photo-2 "
+          />
+          </div>
+          <h3 class="mx-auto hidden 2xl:block mmi-h1">MERCI !!</h3>
+        </div>
         </div>
       </div>
       </div>
