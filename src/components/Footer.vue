3b6f2228026e3e5f7b41f1c1fd0376dd35d15dd5
gros-eliot <95086520+gros-eliot@users.noreply.github.com>
2023-01-15 18:04:40 +0100

    fichier dist correction

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,14 +9,14 @@
       <div class="flex gap-5 items-center">
         <a href="https://discord.gg/nvNsDkZERR">
           <img
-            src="../../public/discord.png"
+            src="/images/discord.png"
             alt="logo discord"
             class="pt-10 md:pt-0"
           />
         </a>
         <a href="https://www.instagram.com/mmi25ans/">
           <img
-            src="../../../public/instagram.svg"
+            src="/images/instagram.svg"
             class="pt-10 md:pt-0"
             alt="logo instagram"
           />
