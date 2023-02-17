7425156c4b26f9c16cb515e8f5928aabf018b29b
gros-eliot <95086520+gros-eliot@users.noreply.github.com>
2023-02-17 15:58:41 +0100

    ajout des oeuvres

--- src/components/OeuvreComponent.vue
+++ src/components/OeuvreComponent.vue
@@ -2,7 +2,7 @@
   <button>
     <section
       :style="{
-        backgroundImage: `url('/images/oeuvres/${sourceImage}')`,
+        backgroundImage: `url('/images/oeuvres/tests/${sourceImage}')`,
       }"
       class="w-full h-full bg-cover bg-center"
       @mouseenter="descriptionVisible = !descriptionVisible"
@@ -59,7 +59,7 @@
             Voter pour cette oeuvre ♥
           </button>
         </RouterLink>
-        <img :src="`/images/oeuvres/${sourceImage}`" class="w-full" />
+        <img :src="`/images/oeuvres/tests/${sourceImage}`" class="w-full" />
         <h3 class="mmi-h2">
           {{ nom }}
         </h3>
